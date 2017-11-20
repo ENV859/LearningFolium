cf4ef778ea2473635f5c3a3cced5173e92d3abdd
john.fay@duke.edu <john.fay@duke.edu>
2017-11-20 11:02:49 -0500

    Stashes old examples

--- sandbox/Fol2.py
+++
@@ -1,8 +0,0 @@
-import folium
-
-map_3 = folium.Map(
-    location=[46.1991, -122.1889],
-    tiles='Stamen Terrain',
-    zoom_start=13)
-map_3.add_child(folium.LatLngPopup())
-map_3.save("map3.html")
