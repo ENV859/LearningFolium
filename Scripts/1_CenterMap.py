620a506ec60cdcddcdce806683088da832c33363
John Fay <john.fay@duke.edu>
2018-11-16 15:13:39 -0500

    Removes old scripts

--- Scripts/1_CenterMap.py
+++
@@ -1,8 +0,0 @@
-#Import the module
-import folium
-
-#Create a map object, centered at the coordinates we provide
-m = folium.Map(location=[45.5236, -122.6750])
-
-#Save the map to a file
-m.save('myMap.html')
